c9420107631da171adba85f10a5d2fd0f52f1f52
GUO <819050014@qq.com>
2019-08-04 17:14:07 +0800

    “你好啊”

---
+++ MyGame/Assets/Script/View/UIPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIPanel : MonoBehaviour {
+    
+
+    protected virtual void Start()
+    {
+        InitViews();
+        InitListener();
+    }
+
+    protected abstract void InitViews();
+
+    protected abstract void InitListener();
+}
---
+++ MyGame/Assets/UIBackGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBackGround : UIPanel {
+    private GameObject UIBackGroundS;
+
+
+    // Use this for initialization
+   protected override void Start () {
+        InitListener();
+        InitViews();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UIBackGroundS.transform.Translate(Vector3.back*Time.deltaTime);
+    }
+    protected override void InitViews()
+    {
+        UIBackGroundS = GameObject.Find("UIBackGround").gameObject;
+    }
+    protected override void InitListener()
+    {
+
+    }
+    
+   
+}
