16d22a4bd1fcf2c733f9598593401a3f91cc9ff9
vintans32123 <31263148+vintans32123@users.noreply.github.com>
2019-08-31 23:27:03 +0800

    继续更新

---
+++ MyGame/Assets/Vintans/_Scripts/Role/AI/AIController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Core;
+
+public class AIController : MonoBehaviour
+{
+    protected Role pRole { get; set; }
+    protected List<AICondition> Conditions = new List<AICondition>();
+    protected StateMachine machine = new StateMachine();
+    [SerializeField]
+    protected string curStateName;
+    
+    private void Start()
+    {
+        pRole = GetComponent<Role>();
+        
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        RegisterEvents();
+        InitConditions();
+        InitAIState();
+    }
+
+    /// <summary>
+    /// 初始化注册事件
+    /// </summary>
+    public virtual void RegisterEvents() { }
+
+    //初始化AI条件
+    public virtual void InitConditions() { }
+    //初始化AI状态
+    public virtual void InitAIState() { }
+    public void Update()
+    {
+        if (machine != null)
+        {
+            machine.OnUpdate();
+        }
+
+        foreach (var item in Conditions)
+        {
+            if (item.OnTrigger())
+            {
+                ChangeState(item.pAIStateType);
+                break;
+            }
+        }
+    }
+
+    public void ChangeState(EnumManager.AIStateType type)
+    {
+        curStateName = type.ToString();
+        machine.GoTo(curStateName);
+    }
+}
